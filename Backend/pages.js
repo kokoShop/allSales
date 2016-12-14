d143b819338060f2ed9a8e45e25a5715ebf49785
Denis Shergin <denisweeks@gmail.com>
2016-12-14 21:14:50 +0200

    Update
    update

--- Backend/pages.js
+++
@@ -1,9 +0,0 @@
-
-
-exports.mainPage = function(req, res){
-	res.render('mainPage');
-};
-
-exports.catalog = function(req, res){
-	res.render('catalog');
-};
--- server.js
+++
@@ -1,3 +0,0 @@
-var main = require('./Backend/main');
-
-main.startServer(5050);
